103cbd30b4ca9b0163769820a4af183ff35e170e
Владислав Кретов <whatislove@MacBook-Air--Whatislovv.local>
2025-05-23 06:09:17 +0200

    Initial polished version with full visuals

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import SuccinctIDCard from './SuccinctIDCard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SuccinctIDCard />
+  </React.StrictMode>
+)
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        pinkcard: '#f472b6',
+        pinkdeep: '#ec4899',
+        pinkborder: '#f9a8d4'
+      },
+      fontFamily: {
+        mono: ['Courier New', 'monospace']
+      }
+    },
+  },
+  plugins: [],
+}
